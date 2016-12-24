70c6f7515e9569962bb5cf26fe977973d8e6674c
c1547497 <simmonsc@cardiff.ac.uk>
2016-12-24 01:31:49 +0000

    created rawModel, VAO storing VBOs etc

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,6 +2,9 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 /**
  * Created by c1547497 on 24/12/2016.
@@ -16,13 +19,32 @@ public class MainGameLoop {
 
         DisplayManager.createDisplay();
 
-        while(!Display.isCloseRequested()){
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
 
+        //OpenGL expects vertices to be defined counter-clockwise by default
+        float[] vertices = {
+            //left bottom triangle
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+            //right top triangle
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f
+        };
+
+        RawModel model = loader.loadToVAO(vertices);
+
+        while(!Display.isCloseRequested()){
+            renderer.prepare();
             //Game logic
             //Rendering
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
 
+        loader.cleanUp();
         DisplayManager.closeDisplay();
     }
 }
--- src/renderEngine/RawModel.java
+++ src/renderEngine/RawModel.java
@@ -4,4 +4,21 @@ package renderEngine;
  * Created by c1547497 on 24/12/2016.
  */
 public class RawModel {
+
+    private int vaoID;
+    private int vertexCount;
+
+    public RawModel(int vaoID, int vertexCount){
+        this.vaoID = vaoID;
+        this.vertexCount = vertexCount;
+    }
+
+    public int getVaoID() {
+        return vaoID;
+    }
+
+    public int getVertexCount() {
+        return vertexCount;
+    }
+
 }
