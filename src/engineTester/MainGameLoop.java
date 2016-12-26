ee353fb64a447423187f5b02963098085baad0bd
c1547497 <simmonsc@cardiff.ac.uk>
2016-12-26 21:01:09 +0000

    optimised rendering process

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,11 +6,8 @@ import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
+import renderEngine.*;
 import models.RawModel;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
 
@@ -28,7 +25,6 @@ public class MainGameLoop {
         DisplayManager.createDisplay();
         Loader loader = new Loader();
         StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
 
         //OpenGL expects vertices to be defined counter-clockwise by default
 
@@ -43,22 +39,21 @@ public class MainGameLoop {
         Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1);
         Light light = new Light(new Vector3f(0,0,-1), new Vector3f(1,1,1));
         Camera camera = new Camera();
-
+        MasterRenderer renderer = new MasterRenderer();
         while(!Display.isCloseRequested()){
             //Game logic
             //Rendering
             entity.increaseRotation(0,1,0);
             camera.move();
-            renderer.prepare();
+            renderer.processEntity(entity);
+            renderer.render(light, camera);
             shader.start();
             shader.loadLight(light);
             shader.loadViewMatrix(camera);
-            renderer.render(entity, shader);
             shader.stop();
             DisplayManager.updateDisplay();
         }
-
-        shader.cleanUp();
+        renderer.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
     }
