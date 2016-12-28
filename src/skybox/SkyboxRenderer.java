13b0d5d7b41670ee9c300343654f7b3f8abfd10b
xChaosMods <azurefury@live.co.uk>
2016-12-28 04:32:03 +0000

    Fixed skybox rotation speed, made days and nights longer (approx 15 mins)

--- src/skybox/SkyboxRenderer.java
+++ src/skybox/SkyboxRenderer.java
@@ -67,7 +67,7 @@ public class SkyboxRenderer {
     private int texture;
     private int nightTexture;
     private SkyboxShader shader;
-    private float time =0;
+    private float time = 0;
 
     public SkyboxRenderer(Loader loader, Matrix4f projectionMatrix){
         cube = loader.loadToVAO(VERTICES, 3);
@@ -95,26 +95,26 @@ public class SkyboxRenderer {
 
     private void bindTextures(){
         time += DisplayManager.getFrameTimeSeconds() * 1000;
-        time %= 24000;
+        time %= 2400000;
         int texture1;
         int texture2;
         float blendFactor;
-        if(time >= 0 && time < 5000){
+        if(time >= 0 && time < 500000){
             texture1 = nightTexture;
             texture2 = nightTexture;
-            blendFactor = (time - 0)/(5000 - 0);
-        }else if(time >= 5000 && time < 8000){
+            blendFactor = (time - 0)/(500000 - 0);
+        }else if(time >= 500000 && time < 800000){
             texture1 = nightTexture;
             texture2 = texture;
-            blendFactor = (time - 5000)/(8000 - 5000);
-        }else if(time >= 8000 && time < 21000){
+            blendFactor = (time - 500000)/(800000 - 500000);
+        }else if(time >= 800000 && time < 2100000){
             texture1 = texture;
             texture2 = texture;
             blendFactor = (time - 8000)/(21000 - 8000);
         }else{
             texture1 = texture;
             texture2 = nightTexture;
-            blendFactor = (time - 21000)/(24000 - 21000);
+            blendFactor = (time - 2100000)/(2400000 - 2100000);
         }
 
         GL13.glActiveTexture(GL13.GL_TEXTURE0);
